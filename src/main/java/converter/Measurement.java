153fff034b9d5ef47aa86d97586fc2dde288fb14
YannMarchelli <yann.marchelli@etu.univ-cotedazur.fr>
2023-04-26 17:01:31 +0200

    Changes to be able to merge

--- src/main/java/converter/Measurement.java
+++ src/main/java/converter/Measurement.java
@@ -2,7 +2,7 @@ package converter;
 
 import java.math.BigDecimal;
 
-public abstract class Measurement {
+public abstract class Measurement { 
     public static BigDecimal printConversion(double res, String u1, String u2) {
         return null;
     }
