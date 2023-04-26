fe3e264533735c0d4691ca4ff3f7105a29509c5e
YannMarchelli <yann.marchelli@etu.univ-cotedazur.fr>
2023-04-26 17:05:05 +0200

    Changes to be able to merge

--- src/test/java/converter/TestEnergy.java
+++ src/test/java/converter/TestEnergy.java
@@ -6,7 +6,7 @@ import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TestEnergy { 
+public class TestEnergy {
     @Test
     public void testToJoules() {
         Energy energy = new Energy("cal");
