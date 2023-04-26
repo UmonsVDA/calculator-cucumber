cbbca5162eadd540c30c804e56fd6fbc23b8d978
YannMarchelli <yann.marchelli@etu.univ-cotedazur.fr>
2023-04-26 17:05:39 +0200

    Changes to be able to merge

--- src/test/java/converter/TestPressure.java
+++ src/test/java/converter/TestPressure.java
@@ -6,7 +6,7 @@ import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TestPressure { 
+public class TestPressure {
     @Test
     public void testToPascals() {
         Pressure pressure = new Pressure("atm");
