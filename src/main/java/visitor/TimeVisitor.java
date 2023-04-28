96f84d855d3105a80615111a82a3cd9ca957bc37
YannMarchelli <yann.marchelli@etu.univ-cotedazur.fr>
2023-04-28 22:27:38 +0200

    Update TimeVisitor.java

--- src/main/java/visitor/TimeVisitor.java
+++ src/main/java/visitor/TimeVisitor.java
@@ -3,7 +3,7 @@ package visitor;
 import calculator.MyTime;
 import calculator.Operation;
 
-public abstract class TimeVisitor implements TimeVisitorInterface {
+public abstract class TimeVisitor {
     /**
      * The Visitor can traverse a number (a subtype of Expression)
      *
@@ -15,5 +15,6 @@ public abstract class TimeVisitor implements TimeVisitorInterface {
      * The Visitor can traverse an operation (a subtype of Expression)
      *
      * @param o The operation being visited
-     */   public abstract void visit(Operation o);
+     */
+    public abstract void visit(Operation o);
 }
