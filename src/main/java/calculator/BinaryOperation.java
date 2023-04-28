bf47a13fbaced16d46644740e6c6165eb0bfedd3
YannMarchelli <74134964+YannMarchelli@users.noreply.github.com>
2023-04-28 15:55:51 +0200

    Update BinaryOperation.java

--- src/main/java/calculator/BinaryOperation.java
+++ src/main/java/calculator/BinaryOperation.java
@@ -5,18 +5,18 @@ import visitor.Visitor;
 import java.util.List;
 
 public abstract class BinaryOperation extends Operation{
-    protected BinaryOperation(List<Expression> elist) throws IllegalOperationException {
+    protected BinaryOperation(List<Expression> elist) throws UnsupportedOperationException, IllegalConstruction {
         super(elist);
     }
 
-    protected BinaryOperation(List<Expression> elist, Notation n) throws IllegalOperationException {
+    protected BinaryOperation(List<Expression> elist, Notation n) throws UnsupportedOperationException, IllegalConstruction {
         super(elist, n);
     }
 
     @Override
-    public void accept(Visitor v) throws IllegalOperationException {
+    public void accept(Visitor v) throws UnsupportedOperationException {
         if (args.size() != 2) {
-            throw new IllegalOperationException();
+            throw new UnsupportedOperationException();
         }
         super.accept(v);
     }
