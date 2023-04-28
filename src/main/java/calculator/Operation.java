20f7495bd1f66c4177d7cf93d22a0a4e131fdc4f
YannMarchelli <74134964+YannMarchelli@users.noreply.github.com>
2023-04-28 16:10:37 +0200

    Update Operation.java

--- src/main/java/calculator/Operation.java
+++ src/main/java/calculator/Operation.java
@@ -1,12 +1,13 @@
 package calculator;
 
+import visitor.Displayer;
+import visitor.TimeVisitor;
 import visitor.Visitor;
 
 import java.math.BigDecimal;
 import java.math.MathContext;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
 
 /**
  * Operation is an abstract class that represents arithmetic operations,
@@ -15,226 +16,211 @@ import java.util.stream.Stream;
  * @see Expression
  * @see MyNumber
  */
-public abstract class Operation implements Expression {
-    /**
-     * The list of expressions passed as an argument to the arithmetic operation
-     */
-    protected final List<Expression> args;
+public abstract class Operation implements Expression
+{
+	/**
+	 * The list of expressions passed as an argument to the arithmetic operation
+	 */
+	public List<Expression> args;
+	/**
+	 * The notation used to render operations as strings.
+	 * By default, the infix notation will be used.
+	 */
+	public Notation notation = Notation.INFIX;
+	/**
+	 * The character used to represent the arithmetic operation (e.g. "+", "*")
+	 */
+	protected String symbol;
 
-    /**
-     * The notation used to render operations as strings.
-     * By default, the infix notation will be used.
-     */
-    private Notation notation;
-    /**
-     * The character used to represent the arithmetic operation (e.g. "+", "*")
-     */
-    protected String symbol;
-    /**
-     * The neutral element of the operation (e.g. 1 for *, 0 for +)
-     */
-    protected int neutral;
-    /**
-     * MathContext object contain the precision and the rounding method to be used for real numbers
-     * By default, the precision is unlimited and the rounding mode is HALF_UP
-     */
-    protected MathContext mathContext = MathContext.UNLIMITED;
+	/**
+	 * The neutral element of the operation (e.g. 1 for *, 0 for +)
+	 */
+	protected int neutral;
 
-    /**
-     * It is not allowed to construct an operation with a null list of expressions.
-     * Note that it is allowed to have an EMPTY list of arguments.
-     *
-     * @param elist The list of expressions passed as argument to the arithmetic operation
-     * @throws IllegalOperationException Exception thrown if a null list of expressions is passed as argument
-     */
-    protected /*constructor*/ Operation(List<Expression> elist) throws IllegalOperationException {
-        this(elist, null);
-    }
+	/**
+	 * MathContext object contain the precision and the rounding method to be used for real numbers
+	 * By default, the precision is unlimited and the rounding mode is HALF_UP
+	 */
+	protected MathContext mathContext = MathContext.UNLIMITED;
 
-    /**
-     * To construct an operation with a list of expressions as arguments,
-     * as well as the Notation used to represent the operation.
-     *
-     * @param elist The list of expressions passed as argument to the arithmetic operation
-     * @param n     The notation to be used to represent the operation
-     * @throws IllegalOperationException Exception thrown if a null list of expressions is passed as argument
-     */
-    protected /*constructor*/ Operation(List<Expression> elist, Notation n) throws IllegalOperationException {
-        if (elist == null) {
-            throw new IllegalOperationException();
-        } else {
-            args = new ArrayList<>(elist);
-        }
-        notation = n == null ? Notation.INFIX : n;
-    }
+	/** It is not allowed to construct an operation with a null list of expressions.
+	 * Note that it is allowed to have an EMPTY list of arguments.
+	 *
+	 * @param elist	The list of expressions passed as argument to the arithmetic operation
+	 * @throws IllegalConstruction	Exception thrown if a null list of expressions is passed as argument
+	 */
+	protected /*constructor*/ Operation(List<Expression> elist)
+			throws IllegalConstruction
+	{
+		this(elist, null);
+	}
 
-    /**
-     * getter method to return the number of arguments of an arithmetic operation.
-     *
-     * @return The number of arguments of the arithmetic operation.
-     */
-    public List<Expression> getArgs() {
-        return args;
-    }
+	/** To construct an operation with a list of expressions as arguments,
+	 * as well as the Notation used to represent the operation.
+	 *
+	 * @param elist	The list of expressions passed as argument to the arithmetic operation
+	 * @param n 	The notation to be used to represent the operation
+	 * @throws IllegalConstruction	Exception thrown if a null list of expressions is passed as argument
+	 */
+	protected /*constructor*/ Operation(List<Expression> elist,Notation n)
+			throws IllegalConstruction
+	{
+		if (elist == null) {
+			throw new IllegalConstruction(); }
+		else {
+			args = new ArrayList<>(elist);
+		}
+		if (n!=null) notation = n;
+	}
 
-    /**
-     * Abstract method representing the actual binary arithmetic operation to compute
-     *
-     * @param l first argument of the binary operation
-     * @param r second argument of the binary operation
-     * @return result of computing the binary operation
-     */
-    public abstract int op(int l, int r) throws IllegalOperationException;
-    // the operation itself is specified in the subclasses
+	/**
+	 * getter method to return the number of arguments of an arithmetic operation.
+	 *
+	 * @return	The number of arguments of the arithmetic operation.
+	 */
+	public List<Expression> getArgs() {
+		return args;
+	}
 
-    /**
-     * Abstract method representing the actual binary arithmetic operation to compute between two real number
-     *
-     * @param l first argument of the binary operation
-     * @param r second argument of the binary operation
-     * @return result of computing the binary operation
-     */
-    public abstract BigDecimal op(BigDecimal l, BigDecimal r) throws IllegalOperationException;
+	/**
+	 * Abstract method representing the actual binary arithmetic operation to compute
+	 * @param l	 first argument of the binary operation
+	 * @param r	second argument of the binary operation
+	 * @return	result of computing the binary operation
+	 */
+	public abstract int op(int l, int r);
+	// the operation itself is specified in the subclasses
+	public MyTime op(MyTime l, MyTime r){
+		throw new UnsupportedOperationException();
+	}
+	/**
+	 * Abstract method representing the actual binary arithmetic operation to time-compute
+	 * @param l	 first argument of the binary operation
+	 * @param seconds	second argument of the binary operation
+	 * @return	result of computing the binary operation
+	 */
+	public MyTime op(MyTime l, MyRealNumber seconds){
+		throw new UnsupportedOperationException();
+	}
 
-    /**
-     * Abstract method representing the actual binary arithmetic operation to compute between two rational numbers
-     *
-     * @param l first rational number of the binary operation
-     * @param r second rational number of the binary operation
-     * @return result of computing the binary operation
-     */
-    public abstract MyRationalNumber op(MyRationalNumber l, MyRationalNumber r) throws IllegalOperationException;
+	/**
+	 * Abstract method representing the actual binary arithmetic operation to compute between two real number
+	 * @param l first argument of the binary operation
+	 * @param r second argument of the binary operation
+	 * @return result of computing the binary operation
+	 */
+	public BigDecimal op(BigDecimal l, BigDecimal r){
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * Abstract method representing the actual binary arithmetic operation to compute between two rational numbers
+	 * @param l	 first rational number of the binary operation
+	 * @param r	second rational number of the binary operation
+	 * @return	result of computing the binary operation
+	 */
+	public MyRationalNumber op(MyRationalNumber l, MyRationalNumber r){
+		throw new UnsupportedOperationException();
+	}
 
 
-    /**
-     * Add more parameters to the existing list of parameters
-     *
-     * @param params The list of parameters to be added
-     */
-    public void addMoreParams(List<Expression> params) {
-        args.addAll(params);
-    }
+	/** Add more parameters to the existing list of parameters
+	 *
+	 * @param params	The list of parameters to be added
+	 */
+	public void addMoreParams(List<Expression> params) {
+		args.addAll(params);
+	}
 
-    public Notation getNotation() {
-        return notation;
-    }
+	/**
+	 * Accept method to implement the visitor design pattern to traverse arithmetic expressions.
+	 * Each operation will delegate the visitor to each of its arguments expressions,
+	 * and will then pass itself to the visitor object to get processed by the visitor object.
+	 *
+	 * @param v	The visitor object
+	 */
+	public void accept(Visitor v) throws UnsupportedOperationException{
+		for(Expression a:args) { a.accept(v); }
+		v.visit(this);
+	}
 
-    public void setNotation(Notation notation) {
-        this.notation = notation;
-    }
+	/**
+   * Convert the arithmetic operation into a String to allow it to be printed,
+   * using the default notation (prefix, infix or postfix) that is specified in some variable.
+   * This method doesn't imply using the same notation for all sub operations.
+   *
+   * @see Expression#printOperation()
+   *
+   * @return	The String that is the result of the conversion.
+   */
+  @Override
+  public final String toString() {
+	  if(Displayer.getInstance()==null) {
+		  Displayer.createDisplayer(true);
+		  String result = accept(Displayer.getInstance());
+		  Displayer.deleteDisplayer();
+		  return result;
+	  }
+	  return accept(Displayer.getInstance());
+  }
 
-    /**
-     * Accept method to implement the visitor design pattern to traverse arithmetic expressions.
-     * Each operation will delegate the visitor to each of its arguments expressions,
-     * and will then pass itself to the visitor object to get processed by the visitor object.
-     *
-     * @param v The visitor object
-     */
-    public void accept(Visitor v) throws IllegalOperationException {
-        if (args.size() == 0) {
-            throw new IllegalOperationException();
-        }
-        for (Expression a : args) {
-            a.accept(v);
-        }
-        v.visit(this);
-    }
 
-    /**
-     * Count the depth of an arithmetic expression recursively,
-     * using Java 8 functional programming capabilities (streams, maps, etc...)
-     *
-     * @return The depth of the arithmetic expression being traversed
-     */
-    public final int countDepth() {
-        // use of Java 8 functional programming capabilities
-        return 1 + args.stream().mapToInt(Expression::countDepth).max().getAsInt();
-    }
+	/**
+	 * Two operation objects are equal if their list of arguments is equal and they correspond to the same operation.
+	 *
+	 * @param o	The object to compare with
+	 * @return	The result of the equality comparison
+	 */
+	@Override
+	public boolean equals(Object o) {
+		if (o == null) return false; // No object should be equal to null
 
-    /**
-     * Count the number of operations contained in an arithmetic expression recursively,
-     * using Java 8 functional programming capabilities (streams, maps, etc...)
-     *
-     * @return The number of operations contained in an arithmetic expression being traversed
-     */
-    public final int countOps() {
-        // use of Java 8 functional programming capabilities
-        return 1 + args.stream().mapToInt(Expression::countOps).reduce(Integer::sum).getAsInt();
-    }
+		if (this == o) return true; // If it's the same object, they're obviously equal
 
-    public final int countNbs() {
-        // use of Java 8 functional programming capabilities
-        return args.stream().mapToInt(Expression::countNbs).reduce(Integer::sum).getAsInt();
-    }
+		if (getClass() != o.getClass()) return false; // getClass() instead of instanceof() because an addition is not the same as a multiplication
 
-    /**
-     * Convert the arithmetic operation into a String to allow it to be printed,
-     * using the default notation (prefix, infix or postfix) that is specified in some variable.
-     *
-     * @return The String that is the result of the conversion.
-     */
-    @Override
-    public final String toString() {
-        return toString(notation);
-    }
+		Operation other = (Operation) o;
+		return this.args.equals(other.getArgs());
+	}
 
-    /**
-     * Convert the arithmetic operation into a String to allow it to be printed,
-     * using the notation n (prefix, infix or postfix) that is specified as a parameter.
-     *
-     * @param n The notation to be used for representing the operation (prefix, infix or postfix)
-     * @return The String that is the result of the conversion.
-     */
-    public final String toString(Notation n) {
-        Stream<String> s = args.stream().map(Object::toString);
-        return switch (n) {
-            case INFIX -> "( " + s.reduce((s1, s2) -> s1 + " " + symbol + " " + s2).get() + " )";
-            case PREFIX -> symbol + " " + "(" + s.reduce((s1, s2) -> s1 + ", " + s2).get() + ")";
-            case POSTFIX -> "(" + s.reduce((s1, s2) -> s1 + ", " + s2).get() + ")" + " " + symbol;
-        };
-    }
+	/** The method hashCode needs to be overridden it the equals method is overridden;
+	 * 	otherwise there may be problems when you use your object in hashed collections
+	 * 	such as HashMap, HashSet, LinkedHashSet.
+	 *
+	 * @return	The result of computing the hash.
+	 */
+	@Override
+	public int hashCode()
+	{
+		int result = 5, prime = 31;
+		result = prime * result + neutral;
+		result = prime * result + symbol.hashCode();
+		result = prime * result + args.hashCode();
+		return result;
+	}
 
-    /**
-     * Two operation objects are equal if their list of arguments is equal and they correspond to the same operation.
-     *
-     * @param o The object to compare with
-     * @return The result of the equality comparison
-     */
-    @Override
-    public boolean equals(Object o) {
-        if (o == null) return false; // No object should be equal to null
+	/**
+	 * Set the MathContext to used to perform operation on real numbers
+	 * @param mc the MathContext to used to perform operation on real numbers
+	 */
+	public void setMathContext(MathContext mc) {
+		mathContext = mc;
+	}
 
-        if (this == o) return true; // If it's the same object, they're obviously equal
+	public String getSymbol() {
+		return symbol;
+	}
 
-        if (getClass() != o.getClass())
-            return false; // getClass() instead of instanceof() because an addition is not the same as a multiplication
+  public void accept(TimeVisitor v){
+	  throw new UnsupportedOperationException();
+  }
 
-        Operation other = (Operation) o;
-        return this.args.equals(other.getArgs());
-    }
+	public Notation getNotation() {
+		return notation;
+	}
 
-    /**
-     * The method hashCode needs to be overridden it the equals method is overridden;
-     * otherwise there may be problems when you use your object in hashed collections
-     * such as HashMap, HashSet, LinkedHashSet.
-     *
-     * @return The result of computing the hash.
-     */
-    @Override
-    public int hashCode() {
-        int result = 5, prime = 31;
-        result = prime * result + neutral;
-        result = prime * result + symbol.hashCode();
-        result = prime * result + args.hashCode();
-        return result;
-    }
+	public void setNotation(Notation notation) {
+		this.notation = notation;
+	}
 
-    /**
-     * Set the MathContext to used to perform operation on real numbers
-     *
-     * @param mc the MathContext to used to perform operation on real numbers
-     */
-    public void setMathContext(MathContext mc) {
-        mathContext = mc;
-    }
 }
